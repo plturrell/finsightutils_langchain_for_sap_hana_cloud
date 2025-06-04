e5dfe9e8e45be1896da1eb8126fc4211c2b792ef
plturrell <plturrell@github.com>
2025-06-04 13:00:20 +0800

    Create simplified app for Vercel deployment

--- api/index.py
+++ api/index.py
@@ -9,8 +9,8 @@ import sys
 # Add the current directory to the path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-# Import the handler from vercel_handler.py
-from vercel_handler import app
+# Import the simplified app for Vercel deployment
+from vercel_app import app
 
 # Make the handler available to Vercel
 handler = app
